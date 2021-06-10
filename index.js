dbc1a5e8216bc5e958a6310102ba7152bdc52707
Joshua Kennedy <josh@outthinkgroup.com>
2021-06-10 13:48:26 -0400

    avoid snake next move

--- index.js
+++ index.js
@@ -24,29 +24,29 @@ function remmy(req, res) {
   });
 }
 
+const allMoves = ["left", "up", "right", "down"];
 function move(req, res) {
-  let moves = ["left", "up", "right", "down"];
+  let moves = [...allMoves];
   const { game, turn, board, you } = req.body;
 
   // ONE MOVE AHEAD
-  moves = avoidSnake(moves, you.body, you.head); // Avoiding ME
   moves = avoidWalls(moves, board.height, you.head); // Avoiding Walls
 
+  moves = avoidSnake(moves, you.body, you.head); // Avoiding ME
   // Avoid other snakes
   const mySnakeId = you.id;
-  board.snakes
-    .filter((snake) => snake.id !== mySnakeId)
-    .forEach((snake) => {
-      moves = avoidSnake(moves, snake.body, you.head); // Avoiding ME
-    });
+  board.snakes.forEach((snake) => {
+    moves = avoidSnake(moves, snake.body, you.head); // Avoiding ME
+    moves = avoidSnakeNextMove(moves, snake.body, you);
+  });
 
+  console.log(moves);
   if (you.health <= 50) {
     moves = toFood(moves, board.food, you.head);
   } else {
     // MOVE TOWARDS TAIL
     moves = toTail(moves, you.body[you.body.length - 1], you.head);
   }
-
   direction = moves[Math.floor(Math.random() * moves.length)];
   console.log(direction);
   res.status(200).json({
@@ -65,7 +65,7 @@ function end() {
 //STRATEGY
 
 function avoidWalls(moves, gameSize, myHead) {
-  const available = availableCoordsFromMoves(moves, myHead); //{move:[x,y]}
+  const available = getMoves(moves, myHead); //{move:[x,y]}
   return Object.keys(available).filter((move) => {
     const { x, y } = available[move];
     return x <= gameSize - 1 && x >= 0 && y <= gameSize - 1 && y >= 0;
@@ -73,7 +73,7 @@ function avoidWalls(moves, gameSize, myHead) {
 }
 
 function avoidSnake(moves, parts, myHead) {
-  const available = availableCoordsFromMoves(moves, myHead); //{move:[x,y]}
+  const available = getMoves(moves, myHead); //{move:[x,y]}
   return Object.keys(available).filter((move) => {
     const { x, y } = available[move];
     if (parts.some((part) => part.x == x && part.y == y)) {
@@ -83,8 +83,25 @@ function avoidSnake(moves, parts, myHead) {
   });
 }
 
+function avoidSnakeNextMove(moves, parts, me) {
+  const snakeHead = parts[0];
+  const myHead = me.head;
+  const snakeLength = parts.length;
+  const myLength = me.body.length;
+
+  if (myLength > snakeLength) return moves; // noop. if Im bigger than move is still safe
+
+  const MyAvailable = getMoves(moves, myHead);
+  return Object.keys(MyAvailable).filter((move) => {
+    const { x: myX, y: myY } = MyAvailable[move];
+    return !Object.keys(getMoves(allMoves, snakeHead)).some(
+      (oSMove) => oSMove.x == myX && oSMove.y == myY
+    );
+  });
+}
+
 function toTail(moves, myTail, myHead) {
-  const available = availableCoordsFromMoves(moves, myHead); //{move:[x,y]}
+  const available = getMoves(moves, myHead); //{move:[x,y]}
   if (!available || !Object.keys(available).length) return [];
   return [
     Object.keys(available).reduce((current, { x, y }) => {
@@ -105,7 +122,7 @@ function toTail(moves, myTail, myHead) {
 }
 
 function toFood(moves, food, head) {
-  const available = availableCoordsFromMoves(moves, head); //{move:[x,y]}
+  const available = getMoves(moves, head); //{move:[x,y]}
   const closestFood = sortByClosest(head, food)[0];
   const moveCoordsToFood = sortByClosest(
     closestFood,
@@ -122,7 +139,7 @@ function toFood(moves, food, head) {
 
 //HELPERS
 
-function availableCoordsFromMoves(moves, coord) {
+function getMoves(moves, coord) {
   return moves.reduce((coords, move) => {
     coords[move] = GO[move.toUpperCase()](coord);
     return coords;
