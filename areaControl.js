c0c5d36e92b9444908c0880f0c09f5695fd7c425
Joshua Kennedy <josh@outthinkgroup.com>
2021-06-11 17:31:02 -0400

    functions wont fail

--- areaControl.js
+++ areaControl.js
@@ -1,5 +1,6 @@
 const { keys } = require("./grid");
 function moveAroundPerimeter(grid, data) {
   //todo
+  return { up: 1 };
 }
 module.exports = { moveAroundPerimeter };
--- otherSnakes.js
+++ otherSnakes.js
@@ -2,15 +2,19 @@ const { keys } = require("./grid");
 
 function smallSnakesNear(grid, data) {
   //todo
+  false;
 }
 function largeSnakeAround(grid, data) {
   //todo
+  return false;
 }
 function avoidLargeSnakes(grid, data) {
   //todo
+  return { up: 1 };
 }
 function hunt(grid, data) {
   //todo
+  return { up: 1 };
 }
 
 module.exports = {
