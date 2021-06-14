c44942df96b038f2891df12ef04bef1e685e6b31
Joshua Kennedy <josh@outthinkgroup.com>
2021-06-14 08:09:44 -0400

    and slinky was born

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 
-app.get("/", remmy);
+app.get("/", slinky);
 app.post("/start", start);
 app.post("/move", (req, res) => {
   direction = move(req);
@@ -18,14 +18,14 @@ app.post("/end", end);
 app.listen(PORT);
 console.log(`go to http://localhost:${PORT}`);
 
-function remmy(req, res) {
+function slinky(req, res) {
   res.status(200).json({
     apiversion: "1",
     author: "joshatoutthink",
-    color: "#399AF2",
-    head: "evil",
-    tail: "bolt",
-    version: "0.0.1-beta",
+    color: "#FD0AFF",
+    head: "silly",
+    tail: "round-bum",
+    version: "final.final.production.final.2.0.-alpha-lol",
   });
 }
 
